acf9248a81d09c180f52c8af63067170210625ea
sergiojanko <janko.sergio1998@gmail.com>
2021-09-14 00:35:04 +0200

    add picture

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <ul>
-      <li>
-        {{movie.title}}
-        {{movie.original_title}}
-        {{movie.original_language}}
-        {{movie.vote_average}}
-      </li>
+      <li>{{movie.title}}</li>
+        <li>{{movie.original_title}}</li>
+        <li v-if="movie.original_language == 'en'"><img src="../assets/images/en.png" alt="en" ></li>
+        <li v-else><img src="../assets/images/it.png" alt="it"></li>
+        <li>{{movie.vote_average}}</li>
+        <li><img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt=""></li>
+      
     </ul>
   </div>
 </template>
@@ -15,6 +16,11 @@
 export default {
 name: "MovieCard",
 props: ["movie"],
+methods: {
+  // getPic(url){
+
+  // }
+}
 // created(){
 //   console.log(this.movie)
 // }
