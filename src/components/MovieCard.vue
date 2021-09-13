60f4edc7a6185c44e619a141274814f85e6cf2d2
sergiojanko <janko.sergio1998@gmail.com>
2021-09-14 00:45:57 +0200

    add stars

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,7 +5,7 @@
         <li>{{movie.original_title}}</li>
         <li v-if="movie.original_language == 'en'"><img src="../assets/images/en.png" alt="en" ></li>
         <li v-else><img src="../assets/images/it.png" alt="it"></li>
-        <li>{{movie.vote_average}}</li>
+        <li><i v-for="n in getVote(movie.vote_average)" :key="n" class="fas fa-star"></i></li>
         <li><img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt=""></li>
       
     </ul>
@@ -17,9 +17,9 @@ export default {
 name: "MovieCard",
 props: ["movie"],
 methods: {
-  // getPic(url){
-
-  // }
+getVote(vote){
+  return Math.ceil(vote/2)
+}
 }
 // created(){
 //   console.log(this.movie)
