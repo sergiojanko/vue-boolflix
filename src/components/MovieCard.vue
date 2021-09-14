c6cbbc18017cb3e8544e572f69be553dbb2a8dcc
sergiojanko <janko.sergio1998@gmail.com>
2021-09-14 15:15:03 +0200

    print five stars

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,7 +5,7 @@
         <li>{{movie.original_title}}</li>
         <li v-if="movie.original_language == 'en'"><img src="../assets/images/en.png" alt="en" ></li>
         <li v-else><img src="../assets/images/it.png" alt="it"></li>
-        <li><i v-for="n in getVote(movie.vote_average)" :key="n" class="fas fa-star"></i></li>
+        <li><i v-for="n in 5" :key="n" class="fa-star" :class="(n < vote) ? 'fas' : 'far'"></i></li>
         <li><img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt=""></li>
       
     </ul>
@@ -20,6 +20,9 @@ methods: {
 getVote(vote){
   return Math.ceil(vote/2)
 }
+},
+computed: {
+  vote(){return Math.ceil(this.movie.vote_average / 2)},
 }
 // created(){
 //   console.log(this.movie)
